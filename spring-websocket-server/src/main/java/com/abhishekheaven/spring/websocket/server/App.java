75a090530d07687d1d3a7e419f78faae9fecc39a
akuma110 <abhishek.kumar22@microfocus.com>
2019-03-24 00:32:53 +0530

    Adding Application class

---
+++ spring-websocket-server/src/main/java/com/abhishekheaven/spring/websocket/server/App.java
@@ -0,0 +1,10 @@
+package com.abhishekheaven.spring.websocket.server;
+
+import org.springframework.boot.SpringApplication;
+
+public class App {
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(App.class);
+        app.run(args);
+    }
+}
