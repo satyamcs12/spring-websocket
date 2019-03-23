c230c958e047020ae7aad8d13b420b206086f493
abhishekheaven7 <abhishekheaven7@gmail.com>
2019-03-24 00:44:01 +0530

    adding sys out

--- spring-websocket-server/src/main/java/com/abhishekheaven/spring/websocket/server/App.java
+++ spring-websocket-server/src/main/java/com/abhishekheaven/spring/websocket/server/App.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class App {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(App.class);
+        System.out.println("Starting Spring Application...");
         app.run(args);
     }
 }
